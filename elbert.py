3dccd1f237df299403c198b634acc10b5e30a4e1
Hunter Boyd <hunterjsb@gmail.com>
2022-01-27 13:52:09 -0500

    initial commit

---
+++ elbert.py
@@ -0,0 +1,161 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from reticker import TickerExtractor
+
+from collections import Counter
+import json
+import time
+
+with open('resource/secrets.json', 'r') as s_f:
+    credentials = json.load(s_f)
+
+
+class Elbert:
+    def __init__(self, user_credentials: dict or None):
+        # Use the `install()` method to set `executable_path` in a new `Service` instance:
+        if user_credentials:  # pass None to `user_credentials` if you don't want to use the WebDriver
+            service = Service(executable_path=ChromeDriverManager().install())
+            self.driver = webdriver.Chrome(service=service,
+                                           options=self._build_options(),
+                                           desired_capabilities=self._build_caps())
+
+            self.email = user_credentials['email']
+            self.password = user_credentials['password']
+            self.online = True
+        else:
+            self.driver = None
+
+        self.logged_in = False
+        self.msg_cache = []
+
+        self.common_phrases = ['IPO', 'ITM', 'OTM', 'BETA', 'GAMMA', 'THETA', 'EPS', 'AM', 'PM', 'USD', 'AH', 'HALTED'
+                               'EST', 'UK', 'RIP', 'HTB', 'IMO', 'OTC', 'FDA', 'FED', 'EOD', 'DD']
+
+    @staticmethod
+    def _build_options():
+        """create Options object to pass to ChromeDriver"""
+        opts = Options()
+        opts.add_argument("auto-select-tab-capture-source-by-title=Screen 1")  # doesn't work
+        opts.add_experimental_option('prefs', {'profile.default_content_setting_values.media_stream_mic': 1})  # mic
+        return opts
+
+    @staticmethod
+    def _build_caps():
+        """create DesiredCapabilities object to pass to ChromeDriver"""
+        caps = DesiredCapabilities.CHROME
+        caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+        return caps
+
+    def login(self):
+        """logs into Discord"""
+        self.driver.get('https://discord.com/login')
+
+        # enter credentials
+        email_input = self.driver.find_element(By.NAME, 'email')
+        password_input = self.driver.find_element(By.NAME, 'password')
+        email_input.send_keys(self.email)
+        password_input.send_keys(self.password)
+
+        # click login
+        self.driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div/div/div/div/form/div/div/div[1]/div[2]/'
+                                           'button[2]/div').click()
+        self.logged_in = True
+
+    def join_atlas(self):
+        """join atlas trading text channel, trading floor 1"""
+        nsfw_continue_button = '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[3]/div[2]/div[1]/' \
+                               'div[1]/div/div[4]/button[2]'  # problem child
+
+        if not self.logged_in:
+            self.login()
+        time.sleep(1)  # replace with webdriver EC
+        self.driver.get('https://discord.com/channels/428232997737594901/697135489484062762')  # xan nation
+        WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, nsfw_continue_button)))
+        time.sleep(1)  # replace with try... except above - click sometimes get interrupted
+        self.driver.find_element(By.XPATH, nsfw_continue_button).click()
+
+    def get_logs(self):
+        """extract http requests from logs"""
+        logs_raw = self.driver.get_log("performance")
+        logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+        return logs
+
+    @staticmethod
+    def log_filter(log_data):
+        """used when determining which logs are responses"""
+        return (
+            # is an actual response
+            log_data["method"] == "Network.responseReceived"
+            # and json
+            and "json" in log_data["params"]["response"]["mimeType"]
+        )
+
+    def _save_cache(self):
+        with open('resource/msg_cache.json', 'w') as f:
+            json.dump(self.msg_cache, f, indent=4)
+
+    def _load_cache(self):
+        with open('resource/msg_cache.json') as f:
+            self.msg_cache = json.load(f)
+        return self.msg_cache
+
+    def load_messages(self):
+        """
+        request logs then get responses for the messages.
+        saves the last 50 messages retrieved.
+        if Elbert is offline it will load the saved cache.
+        """
+        if not self.driver:
+            return self._load_cache()
+
+        self.join_atlas()
+        responses = []
+        _logs = self.get_logs()
+
+        for _log in filter(self.log_filter, _logs):
+            request_id = _log["params"]["requestId"]
+            resp_url = _log["params"]["response"]["url"]
+            print(f"Caught {resp_url}")
+            if 'messages' in resp_url:
+                msg_cache = self.driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})['body']
+                self.msg_cache = json.loads(msg_cache)
+                responses.append(self.msg_cache)
+
+        self._save_cache()
+        return responses
+
+    def parse_cache(self, parser: TickerExtractor):
+        tickers_ = []
+        for i, msg in enumerate(self.msg_cache):
+            msg_tickers = parser.extract(msg['content'])
+            self.msg_cache[i]['tickers'] = msg_tickers
+            tickers_ += msg_tickers
+
+        tickers_ = Counter(tickers_)
+        for phrase in self.common_phrases:
+            if phrase in tickers_:
+                tickers_.pop(phrase)
+
+        self._save_cache()
+        return tickers_
+
+
+########################################################################################################################
+if __name__ == "__main__":
+
+    # join: atlas trading / trading floor 1
+    elbert = Elbert(None)
+    # elbert.join_atlas()
+
+    # open the messages
+    elbert.load_messages()
+    tickers = elbert.parse_cache(TickerExtractor())  # Counter by default
+    print(tickers)
---
+++ plotter.py
@@ -0,0 +1,122 @@
+from elbert import Elbert
+
+from reticker import TickerExtractor
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+
+import json
+
+with open('resource/secrets.json') as s_f:
+    credentials = json.load(s_f)
+
+
+class Plotter:
+    def __init__(self, creds=None):
+        if creds:
+            self.api_key = creds['polygon api key']
+
+        self.stock_cache = None
+
+    def _cache_stock(self):
+        with open('resource/stock_cache.json', 'w') as f:
+            json.dump(self.stock_cache, f, indent=4)
+
+    def _load_cache(self):
+        with open('resource/stock_cache.json') as f:
+            self.stock_cache = json.load(f)
+
+    def get_stock(self, ticker: str, timespan: str, date_range: list, multiplier=1, **kwargs):
+        """
+        polygon.io endpoint: /v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from}/{to}
+        kwargs are: adjusted, sort, limit
+        returns  and caches a json list of agg. data
+        """
+        stock_url = (f"https://api.polygon.io/v2/aggs/ticker/"
+                     f"{ticker}/range/{multiplier}/{timespan}/"
+                     f"{date_range[0]}/{date_range[1]}?")
+
+        if kwargs:
+            for k, v in kwargs.items():
+                stock_url += f"{k}={v}&"
+        stock_url += f"apiKey={self.api_key}"
+
+        self.stock_cache = requests.get(stock_url).json()
+        self._cache_stock()
+        return self.stock_cache
+
+    def top_stocks(self, stock_counts: dict, n_stocks: int = 3):
+        """
+        takes in a dict of the most mentioned tickers & their frequencies.
+        generates n + 1 plots, plot 0 being a bar chart of the most mentioned ticker.
+        charts 1:n will be the price top n tickers.
+        """
+        fig, ax = plt.subplots(n_stocks+1)
+
+        df = pd.DataFrame(stock_counts.items(), columns=['ticker', 'frequency'])\
+            .sort_values(['frequency'], ascending=False, ignore_index=True)
+        ax[0].bar(df[:10]['ticker'], df[:10]['frequency'])
+        ax[0].set_title('Top 10 Mentioned Tickers')
+
+        for i in range(n_stocks):
+            print(df['ticker'])
+            self.get_stock(ticker=df['ticker'][i],
+                           date_range=["2021-10-26", "2022-01-26"],
+                           timespan='day')
+            if 'results' in self.stock_cache:
+                data = pd.DataFrame(self.stock_cache['results'])
+                up = data[data.c <= data.o]
+                down = data[data.c > data.o]
+
+                self.candlestick_chart(ax[i+1])
+                ax[i+1].set_title(df['ticker'][i])
+            else:
+                print(f"could not retrieve data for {df['ticker'][i]}")
+        plt.show()
+
+    def candlestick_chart(self, axis: plt, prices: pd.DataFrame = None, candle_width=.4, wick_width=.05,
+                          volume_opacity=0.25, scaling_factor=50):
+        """creates a candlestick chart from a pandas """
+        if not prices:
+            if self.stock_cache:
+                prices = pd.DataFrame(self.stock_cache['results'])
+            else:
+                self._load_cache()
+                prices = pd.DataFrame(self.stock_cache['results'])
+
+        # define up and down prices
+        up = prices[prices.c >= prices.o]
+        down = prices[prices.c < prices.o]
+
+        scaled_low = prices.l.min() / scaling_factor
+        avg_vol = prices.v.mean()
+        scaling_factor = avg_vol / scaled_low  # to divide volume by
+
+        # define colors to use
+        col1, col2 = 'green', 'red'
+
+        # plot up prices & volume
+        axis.bar(up.index, up.c - up.o, candle_width, bottom=up.o, color=col1)
+        axis.bar(up.index, up.h - up.c, wick_width, bottom=up.c, color=col1)
+        axis.bar(up.index, up.l - up.o, wick_width, bottom=up.o, color=col1)
+        axis.bar(up.index, up.v / scaling_factor, bottom=prices.l.min(), color=col1, alpha=volume_opacity)
+
+        # plot down prices & volume
+        axis.bar(down.index, down.c - down.o, candle_width, bottom=down.o, color=col2)
+        axis.bar(down.index, down.h - down.o, wick_width, bottom=down.o, color=col2)
+        axis.bar(down.index, down.l - down.c, wick_width, bottom=down.c, color=col2)
+        axis.bar(down.index, down.v / scaling_factor, bottom=prices.l.min(), color=col2, alpha=volume_opacity)
+
+        return plt
+
+
+#####################################
+if __name__ == "__main__":
+    eb = Elbert(credentials)
+    msgs = eb.load_messages()
+    counts = eb.parse_cache(TickerExtractor())
+    print(counts)
+
+    plotter = Plotter(credentials)
+    # plotter.get_stock('MSFT', 'day', ['2021-12-01', '2022-01-25'])
+    plotter.top_stocks(counts, 3)
